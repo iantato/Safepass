8b3bc223b50dff12b1ef84d45b6259cdee57629e
iantato <83338977+iantato@users.noreply.github.com>
2025-01-18 12:27:19 +0800

    added symmetric key decryption.

--- src/safepass/crypto/key_manager.py
+++ src/safepass/crypto/key_manager.py
@@ -7,29 +7,31 @@ from safepass.config.settings import ITERATIONS, KEY_LENGTH
 
 class KeyManager:
 
-    def __init__(self, master_password: str, username: str):
+    def __init__(self, master_password: str, username: str, user_exists: bool = False):
         self._key: Optional[bytes] = None
         self._symmetric_key: Optional[bytes] = None
+        self._encrypt_symmetric_key: Optional[bytes] = None
         self._nonce : Optional[bytes] = None
-        self._initialize(master_password, username)
+        if not user_exists:
+            self._initialize(master_password, username)
 
     def _initialize(self, master_password: str, username: str) -> None:
         self._key = self._derive_key(master_password.encode('utf-8'), username.encode('utf-8'))
         self._symmetric_key = os.urandom(32)
         self._nonce = os.urandom(12)
-        self._encrypt_symmetric_key()
+        self._encrypt_symmetric_key(self._symmetric_key, self._nonce)
 
     def get_key(self) -> Optional[bytes]:
         if not self._key:
             raise ValueError("Master key is not set.")
         return self._key
 
-    def get_encrypted_key(self) -> Optional[bytes]:
+    def get_encrypted_key(self) -> bytes:
         if not self._encrypt_symmetric_key:
             raise ValueError("Encrypted symmetric key is not set.")
         return self._encrypt_symmetric_key
 
-    def get_nonce(self) -> Optional[bytes]:
+    def get_nonce(self) -> bytes:
         if not self._nonce:
             raise ValueError("Nonce is not set.")
         return self._nonce
@@ -44,9 +46,19 @@ class KeyManager:
 
         return kdf.derive(master_password)
 
-    def _encrypt_symmetric_key(self) -> bytes:
+    def _encrypt_symmetric_key(self, symmetric_key: bytes, nonce: bytes) -> bytes:
         aesgcm = AESGCM(self._key)
-        self._encrypt_symmetric_key = aesgcm.encrypt(self._nonce, self._symmetric_key, None)
+        self._encrypt_symmetric_key = aesgcm.encrypt(nonce, symmetric_key, None)
+
+    def decrypt_symmetric_key(self, encrypted_key: bytes, nonce: bytes) -> bytes:
+        if not self._key:
+            raise ValueError("Master key is not set.")
+
+        aesgcm = AESGCM(self._key)
+        try:
+            return aesgcm.decrypt(nonce, encrypted_key, None)
+        except Exception as e:
+            raise ValueError(f"Failed to decrypt symmetric key: {str(e)}") from e
 
     def destroy(self) -> None:
         self._key = None
